49635ba3ef1d9adff47a2cb9f925462110580247
liuzhe <779402401@qq.com>
2019-03-26 22:26:55 +0800

    童书馆登录测试用例-po模式

--- src/test/bookstore/LoginFirstTest.java
+++ src/test/bookstore/LoginFirstTest.java
@@ -28,15 +28,15 @@ public class LoginFirstTest {
     }
 
     @Test
-    private void test(){
+    private void test() throws InterruptedException {
         //在浏览器输入地址
 
-        driver.get("https://account.baobaobooks.com/");
+        driver.get("https://fxm5547.baobaobooks.com/");
         System.out.printf(driver.getTitle());
-        if (driver.getTitle().equals("孩宝小镇 - 登录")){
-            System.out.printf("\n进入网站成功");
+        if (driver.getTitle().equals("小明fxm5547")){
+            System.out.printf("\n进入网站成功\n");
         }else {
-            System.out.printf("\n进入网站失败");
+            System.out.printf("\n进入网站失败\n");
         }
 
 
@@ -63,6 +63,30 @@ public class LoginFirstTest {
 //        new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOf(logins_webElement));
 //        logins_webElement.click();
 
+//        点击我的
+        NavinationBar navinationBar = new NavinationBar(driver);
+        new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOf(navinationBar.my_webElement));
+        navinationBar.clickButton1();
+
+        new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOf(navinationBar.title_element));
+        navinationBar.clickButton2();
+
+
+//        By my = By.xpath("//*[@class='icon icon-footer4']");
+//        WebElement my_element = driver.findElement(my);
+//        my_element.click();
+//        Reporter.log("Click我的button", true);
+//        Thread.sleep(3000);
+
+//        进入登录提示页面//
+
+//        By title = By.xpath("//*[@class='btn']");
+//        WebElement title_element = driver.findElement(title);
+//        Reporter.log("Find注册或登录后即可继续操作button", true);
+//        Thread.sleep(3000);
+//        Reporter.log("Click注册或登录后即可继续操作button", true);
+//        title_element.click();
+//        进入登录页面
         LoginFirstPage loginFirstPage = new LoginFirstPage(driver);
         new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOf(loginFirstPage.logins_webElement));
         new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOf(loginFirstPage.userName_webElement));
@@ -73,6 +97,7 @@ public class LoginFirstTest {
         loginFirstPage.loginButton();
 //        System.out.printf(driver.getTitle());
         Assert.assertEquals(driver.getTitle(),"孩宝小镇 - 登录");
+        Thread.sleep(3000);
 
 
         out.println("\npassed!");
