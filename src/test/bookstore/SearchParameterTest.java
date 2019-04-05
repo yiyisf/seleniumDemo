386051c93257dd98be68f6fd9d76ae19027e7e96
liuzhe <779402401@qq.com>
2019-04-05 18:19:59 +0800

    New changelist

---
+++ src/test/bookstore/SearchParameterTest.java
@@ -0,0 +1,88 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liuzhe on 2019/4/4.
+ */
+
+public class SearchParameterTest {
+
+
+    WebDriver driver = null;
+
+    @BeforeMethod
+    private void setup() {
+        //启动chrome浏览器
+        driver = new ChromeDriver();
+
+    }
+    @DataProvider
+   public Object[][] getData(){
+       Object[][] o={{"鹅妈妈"},{"小猪"}};
+       return o;
+
+   }
+
+
+
+    @Test (dataProvider = "getData")
+    private void searchTest(String s) throws InterruptedException {
+        //在浏览器输入地址
+
+
+        driver.get("https://fxm5547.baobaobooks.com/");
+        System.out.printf(driver.getTitle());
+        if (driver.getTitle().equals("小明fxm5547")) {
+            System.out.printf("\n进入网站成功\n");
+        } else {
+            System.out.printf("\n进入网站失败\n");
+        }
+
+        LoginFirstPage loginFirstPage = new LoginFirstPage(driver);
+        loginFirstPage.searchClick();
+
+        SearchPage searchPage = new SearchPage(driver);
+
+        String expectGoodsName = "有试听 廖彩杏书单 英国进口 经典版鹅妈妈童谣合集 My Very First Mother Goose Pack 点读版【超大开本平装】";
+
+
+        SearchResultPage searchResultPage = searchPage.gotoSearchResult(s);
+        Thread.sleep(3000);
+
+
+        List<String> assertMsg = new ArrayList<String>();
+        int i = 1;
+        for(String goods_desc : searchResultPage.returnGoodsname()) {
+            System.err.println("正在校验第" + i++ + "条数据");
+            if(!goods_desc.contains(expectGoodsName)){
+                assertMsg.add("期待的标题不包含:" + expectGoodsName + ",实际的标题是:" + goods_desc +"\r\n");
+            }else {
+
+                System.out.printf("pass");
+            }
+        }
+
+        if(!assertMsg.isEmpty()) {
+            Assert.assertTrue(false, assertMsg.toString());
+        }
+
+
+
+    }
+
+
+    @AfterMethod
+    private void teardown() {
+        driver.quit();
+    }
+
+}
+
