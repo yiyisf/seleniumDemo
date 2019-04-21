68f96623b6f013fb57a24acf083d3cab77f8a43c
liuzhe <779402401@qq.com>
2019-04-21 18:19:03 +0800

    modify testcase path java;

--- src/test/javaDemo/LoginTest.java
+++ src/test/javaDemo/LoginTest.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,10 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
 import static java.lang.System.out;
-import static org.testng.Assert.assertEquals;
 
 /**
  * Created by liuzhe on 2019/2/17.†
